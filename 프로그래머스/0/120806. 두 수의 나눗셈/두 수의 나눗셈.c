34fb047e8be551571a5ba45aa16048ca8a6ec314
KIMYOUNGGEUN <doctor6749@gmail.com>
2025-04-02 00:47:47 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 3.73 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    int answer;
+
+answer = ((float)num1 / (float)num2) * 1000;
+
+
+    return answer;
+}
